7e3adf849e7d4f0536ce9e109eb442837a03314e
Henry Darana <henry.darana@tray.com>
2024-05-07 12:27:48 +0530

    Learn Nano

---
+++ linux-learn-commands/5-nano/lesson-5.sh
@@ -0,0 +1,19 @@
+nano filename.txt       # Opens the file "filename.txt" in the Nano text editor.
+                         # If the file doesn't exist, it creates a new one with that name.
+                         # No output unless an error occurs.
+
+Ctrl + O , and Enter    # Saves the changes made in Nano and confirms the save operation.
+                         # No output unless an error occurs.
+
+Ctrl + X                # Exits the Nano text editor.
+                         # No output unless an error occurs.
+
+nano filename.txt       # Opens the file "filename.txt" in the Nano text editor.
+                         # Since the file doesn't exist, Nano creates a new empty file with that name.
+                         # No output unless an error occurs.
+
+Ctrl + W                # Initiates a search within the Nano text editor.
+                         # No output unless an error occurs.
+
+Ctrl + \                # Initiates a replace operation within the Nano text editor.
+                         # No output unless an error occurs.
